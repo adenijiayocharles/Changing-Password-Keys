5244e4cecaa222485749077ca759cf37872e1b25
Adeniji Charles ayodipupo <adenijiayocharles@gmail.com>
2017-08-05 22:16:11 +0100

    first draft done

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
 (function(){
-
+	//numbers array
 	var numbers = [];
 
+	//random number generator gotten from Mozilla Developer's Network
 	function getRandomNumber(min, max){
 		min = Math.ceil(min);
 		max = Math.floor(max);
 		return Math.floor(Math.random() * (max - min + 1)) + min;
 	}
 
+	// counter
 	var i = 0;
 	while(i <= 10){
 		var number = getRandomNumber(0, 9);
@@ -18,6 +20,12 @@
 			i++
 		}
 	}
-	document.write(numbers);
-	console.log(numbers);
+
+	//create buttons
+	var numbersLength = numbers.length;
+	var buttons = "";
+	for(var i = 0; i < numbersLength; i++){
+		buttons += "<input type='button' name='choices' value="+ numbers[i] +">";
+	}
+	document.write(buttons);
 })();
